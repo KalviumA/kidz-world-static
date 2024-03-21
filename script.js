a622a6ff2c52a15986b3a2a4ac5f5d3be7ac134d
Priyap1038 <124154881+Priyap1038@users.noreply.github.com>
2024-03-21 09:42:23 +0530

    done

---
+++ script.js
@@ -0,0 +1,41 @@
+let cart=[];
+
+let product= document.querySelectorAll(".container>div")
+// console.log(product)
+
+product.forEach((ele)=>{
+    let addbutton=ele.querySelector(".button.flex");
+   
+    addbutton.addEventListener("click",()=>{
+        let obj={
+            name:ele.querySelector("h3"),
+            quantity:1,
+        }
+        cart.push(obj);
+
+  alert("added to the cart")
+    })
+  
+    
+})
+console.log(cart);
+
+
+
+
+// now clickon cart reflect the number of items
+let cartItem=document.getElementById("cart");
+
+
+
+cartItem.addEventListener("click",()=>{
+
+    let value=document.getElementById("cart-value");
+
+    // console.log(value.innerText)
+    value.innerText=cart.length;
+
+    console.log(cart)
+    // console.log(cart.length)
+})
+
